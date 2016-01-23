d70409b3bf45031bd84687eed36de0d2e5823e69
Kurtis Reid <kreid8@kent.edu>
2016-01-23 14:38:08 -0500

    added tag list

--- import_xml.py
+++ import_xml.py
@@ -2,11 +2,30 @@ from urllib2 import urlopen
 from xml.dom import minidom
 from pprint import pprint
 
-ny = urlopen('https://events.stanford.edu/xml/eventlist.xml')
+def print_elements(eles):
+    #prints out elements enclosed in tags
+    for ele in eles:
+        print ele.firstChild.nodeValue.encode('utf-8').strip()
+        
 
-pets = minidom.parseString(ny.read())
-ny.close()
+    
+    
+tag_list = ['title', 'locationText', 'description_textonly', 'url', 'locationText', 'beginDate', 'beginTime', 'endTime', 'tags', 'categories'] #this tag list works with the stanford xml
+    
+    
 
-titles = pets.getElementsByTagName('title')
-for title in titles:
-    print title.firstChild.nodeValue.encode('utf-8').strip()
+university = urlopen('https://events.stanford.edu/xml/eventlist.xml')
+#Upen = urlopen('https://www.sas.upenn.edu/events/rss.xml')
+#ny = urlopen('http://events.bc.edu/calendar.xml')
+data = minidom.parseString(university.read())
+university.close()
+
+titles = data.getElementsByTagName(tag_list[0])
+locationTexts = data.getElementsByTagName(tag_list[1])
+descriptions = data.getElementsByTagName(tag_list[2])
+print_elements(titles)
+print_elements(locationTexts)
+print_elements(descriptions)
+#for title in titles:
+#    print title.firstChild.nodeValue.encode('utf-8').strip()
+#print name.firstChild.nodeValue
