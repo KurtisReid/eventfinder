15403bd228ffadf9ec3717d5101ce87b8ae4a7d2
Kurtis Reid <kreid8@kent.edu>
2016-01-23 13:27:57 -0500

    fixed encoding error

--- import_xml.py
+++ import_xml.py
@@ -1,11 +1,12 @@
 from urllib2 import urlopen
 from xml.dom import minidom
+from pprint import pprint
 
 ny = urlopen('https://events.stanford.edu/xml/eventlist.xml')
 
 pets = minidom.parseString(ny.read())
 ny.close()
 
-title = pets.getElementsByTagName('title')
-for name in names:
-	print name.firstChild.nodeValue
+titles = pets.getElementsByTagName('title')
+for title in titles:
+    print title.firstChild.nodeValue.encode('utf-8').strip()
