3b280368b9dbeb599f05b9791775a3305a90b08d
Kurtis Reid <kreid8@kent.edu>
2016-01-23 16:50:57 -0500

    changed to default tag_list, checking for titles

--- import_xml.py
+++ import_xml.py
@@ -3,16 +3,22 @@ from xml.dom import minidom
 from pprint import pprint
 from collections import defaultdict
 
-def print_elements(eles):
+#def print_elements(eles):
     #prints out elements enclosed in tags
-    for ele in eles:
-        print ele.firstChild.nodeValue.encode('utf-8').strip()
+    #for ele in eles:
+        #print ele.firstChild.nodeValue.encode('utf-8').strip()
+
+def getElements(xmlData, tag_list_number):
+	default_tag_list = ['title', 'description', 'link', 'pubDate']
+	#tag_list = ['title', 'locationText', 'description_textonly', 'url', 'beginDate', 'beginTime', 'endTime', 'tags', 'categories'] #this tag list works with the stanford xml
+	return xmlData.getElementsByTagName(default_tag_list[tag_list_number])
         
 events = defaultdict(list)
 fout = open("output.dat", "w")
     
+
     
-tag_list = ['title', 'locationText', 'description_textonly', 'url', 'locationText', 'beginDate', 'beginTime', 'endTime', 'tags', 'categories'] #this tag list works with the stanford xml
+
     
     
 
@@ -22,17 +28,41 @@ university = urlopen('https://events.stanford.edu/xml/eventlist.xml')
 data = minidom.parseString(university.read())
 university.close()
 
+#TODO: check that the data contains all the correct tags
 
-titles = data.getElementsByTagName(tag_list[0])
-locationTexts = data.getElementsByTagName(tag_list[1])
-descriptions = data.getElementsByTagName(tag_list[2])
+titles = getElements(data, 0)
+#locationTexts = getElements(data, 1)
+#descriptions = getElements(data, 2)
+descriptions = getElements(data, 1)
+
+#url = getElements(data, 3)
+url = getElements(data, 2)
+beginDate = getElements(data, 3)
+#beginDate = getElements(data, 4)
+#beginTime = getElements(data, 5)
+#endTime = getElements(data, 6)
+#event_tags = getElements(data, 7) #note: may be unigue to stanford
+#categories = getElements(data, 8)
 print len(titles)
-print len(locationTexts)
+#print len(locationTexts)
 print len(descriptions)
-for key_event in range(len(titles) - 1):
-	events[key_event].append(titles[key_event + 1].firstChild.nodeValue.encode('utf-8').strip())
-	events[key_event].append(locationTexts[key_event].firstChild.nodeValue.encode('utf-8').strip())
+flag_titles = 0
+if len(titles) > len(descriptions):
+	range_titles = len(titles) - 1
+else:
+	range_titles = len(titles)
+	flag_titles = 1	
+
+for key_event in range(range_titles):
+	if flag_titles:
+		events[key_event].append(titles[key_event].firstChild.nodeValue.encode('utf-8').strip())
+	else:
+		events[key_event].append(titles[key_event + 1].firstChild.nodeValue.encode('utf-8').strip())
+	#events[key_event].append(locationTexts[key_event].firstChild.nodeValue.encode('utf-8').strip())
 	events[key_event].append(descriptions[key_event].firstChild.nodeValue.encode('utf-8').strip())
+	events[key_event].append(url[key_event].firstChild.nodeValue.encode('utf-8').strip())
+	events[key_event].append(beginDate[key_event].firstChild.nodeValue.encode('utf-8').strip())
+
 	
 for val in events.values():
     for element in val:
