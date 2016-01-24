f4ff35a7e584376c37c1256f91148e3f4bb70649
Dor-Ron <dor.rondel@gmail.com>
2016-01-24 05:08:42 -0500

    finished landing section?

--- __init__.py
+++ __init__.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template, url_for, flash, request
-import forms
+from flask import (Flask,
+                   render_template,
+                   url_for,
+                   flash,
+                   request,
+                   g,
+                   session)
 
 app = Flask(__name__)
 app.secret_key = 'REDACTED'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    city = 0
-    category = 0
-    '''
-    form = forms.SearchForm()
-    if form.validate_on_submit():
-        flash("Searching for matching results", "success")
-        city = form.city.data
-        category = form.category.data
-    '''
-    print request.form.get('get-date')
-    return render_template("index.html", title="Event Finder",
-                                         #form = form,
-                                         city=city,
-                                         category=category)
-
+    session['city'] = request.form.get('city')
+    session['category'] = request.form.get('category')
+    session['date'] = request.form.get('date')
+    return render_template("index.html", title="Event Findr")
 
 @app.route('/about/')
 def helloWorld():
-	return render_template("about.html", title="About Us")
+    return render_template("about.html", title="About Us")
+
+@app.route('/results/')
+    return render_template()
 
 if __name__ == '__main__':
     print( " * Your app is running on port 5000!")
--- static/scripts/index.js
+++ static/scripts/index.js
@@ -7,14 +7,3 @@ $('#dp1').fdatepicker({
     format: 'dd M yyyy',
     disableDblClickSelection: true
 });
-
-var alert = {
-	body: '<div style="text-align:center"><h1 style="color:orange; font-family: \'Indie Flower\', cursive; font-size:40px">Contact Information</h1><br/><p style="color:orange; font-family: \'Indie Flower\', cursive; font-size: 25px;">You can e-mail me at<p style="font-size: 30px; color:orange; font-family: \'Indie Flower\', cursive;"> dor.rondel@gmail.com</p></p></div>',
-	buttons: {
-		action: {
-			title: 'Dismiss',
-			fn: basicModal.close
-		}
-	}
-}
-document.querySelector('a.alert').addEventListener('click', function() { basicModal.show(alert) })
