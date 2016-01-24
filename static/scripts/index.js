cc46a10a1347e5008da85834e5b1aad0ddd8d0de
Dor-Ron <dor.rondel@gmail.com>
2016-01-24 03:26:25 -0500

    idk..

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,8 +1,20 @@
 console.log("Just seeing that things work...");
 
+
 $(document).foundation();
 window.prettyPrint && prettyPrint();
 $('#dp1').fdatepicker({
-    format: 'mm-dd-yyyy',
+    format: 'dd M yyyy',
     disableDblClickSelection: true
 });
+
+var alert = {
+	body: '<div style="text-align:center"><h1 style="color:orange; font-family: \'Indie Flower\', cursive; font-size:40px">Contact Information</h1><br/><p style="color:orange; font-family: \'Indie Flower\', cursive; font-size: 25px;">You can e-mail me at<p style="font-size: 30px; color:orange; font-family: \'Indie Flower\', cursive;"> dor.rondel@gmail.com</p></p></div>',
+	buttons: {
+		action: {
+			title: 'Dismiss',
+			fn: basicModal.close
+		}
+	}
+}
+document.querySelector('a.alert').addEventListener('click', function() { basicModal.show(alert) })
